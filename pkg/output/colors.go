d0a24a77b13e754a71f42a00fb9b101454e5e912
Neeraj Sah <neerajsahnx@gmail.com>
2025-05-01 23:43:39 +0530

    Update colors.go

--- pkg/output/colors.go
+++ pkg/output/colors.go
@@ -1,1 +1,14 @@
- 
+package output
+
+import "github.com/fatih/color"
+
+// Define color functions for terminal output
+var (
+	ColorGreen   = color.New(color.FgGreen).SprintFunc()
+	ColorRed     = color.New(color.FgRed).SprintFunc()
+	ColorWhite   = color.New(color.FgWhite).SprintFunc()
+	ColorBlue    = color.New(color.FgBlue).SprintFunc()
+	ColorMagenta = color.New(color.FgMagenta).SprintFunc() // Pink/Magenta
+    ColorYellow  = color.New(color.FgYellow).SprintFunc()  // For warnings or info
+    ColorCyan    = color.New(color.FgCyan).SprintFunc()    // For details like keywords
+) 
